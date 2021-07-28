dabb513091502c0b876d133b813d0d92092643bd
wenyt <wenytang@wenytang.onmicrosoft.com>
2021-07-28 17:55:28 +0800

    chore: update

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 
-if [[ "$HELLO" == "hello" && -z '$(git diff --cached)' ]]; then
-  echo "Hello foo."
-else
-  echo "You are not foo."
+# if [[ "$HELLO" == "hello" && ! -z '$(git diff --cached)' ]]; then
+#   echo "Hello foo."
+# else
+#   echo "You are not foo."
+# fi
+
+# var=${git diff --cached}
+touch a.txt 
+git add .
+
+if [[ ! -z "$(git diff --cached)" ]]
+then 
+    echo 'hello'
 fi
