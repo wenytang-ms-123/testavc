3a09833fdefc2f4b8efd3645dbd98aa30e7e24d8
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-07-30 13:23:10 +0800

    chore: update

--- packages/test1/scripts/postversion.sh
+++ packages/test1/scripts/postversion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ -z \"$(git tag --points-at HEAD | grep templates)\" && ! -z \"$(git diff --cached -- ../../templates)\" ]]
+if [[ -z "$(git tag --points-at HEAD | grep templates)" && ! -z "$(git diff --cached -- ../../templates)" ]]
 then
     echo "=============== templates@$(node -p "require('../../templates/package.json').version")"
     git tag "templates@$(node -p "require('../../templates/package.json').version")"
