86217e02b1c76ce82c54a510d8a95d801f4cb1bb
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-07-30 15:43:36 +0800

    chore: update

--- packages/test1/scripts/version.sh
+++ packages/test1/scripts/version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -z "$(git diff -- ../../templates)" ]; then
 echo '======== help bump up'
-node ../../templates/scripts/sync-up-version.js yes;
+node ./scripts/sync-up-version.js yes;
 else 
 echo '======== self bump up'
-node ../../templates/scripts/sync-up-version.js 
+node ./scripts/sync-up-version.js 
 fi
 git add ../../templates
