00175d677c684ca88420018100fb3b4d0de6a357
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-07-30 13:47:39 +0800

    chore: update

--- packages/test1/scripts/postversion.sh
+++ packages/test1/scripts/postversion.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 if [[ -z "$(git tag --points-at HEAD | grep templates)" && ! -z "$(git diff HEAD^ -- ../../templates/package.json|grep version)" ]]
 then
-    echo "=============== templates@$(node -p "require('../../templates/package.json').version")"
     git tag "templates@$(node -p "require('../../templates/package.json').version")"
 fi
--- templates/scripts/sync-up-version.js
+++ templates/scripts/sync-up-version.js
@@ -24,6 +24,7 @@ function listFile(dir, list = []) {
 
 const template_dir = path.join(__dirname, "..");
 const dep_pkgs = listFile(template_dir);
+let change = false;
 for (file of dep_pkgs) {
   let pkg_ = loadJsonFile.sync(file);
   let dep = pkg_.dependencies;
@@ -35,8 +36,22 @@ for (file of dep_pkgs) {
       } else if (!semver.intersects(dep_map.get(sdk_name), sdk_version)) {
         dep_map.set(sdk_name, `^${sdk_version}`);
       }
+      else continue;
+      change = true;
       pkg_.dependencies = Object.fromEntries(dep_map);
       writePkg(file, pkg_);
     }
   }
+}
+
+let needBumpUp = process.argv0 === 'yes' ? true: false;
+if(change && needBumpUp) {
+  let file = path.join(template_dir, "package.json")
+  console.log('path======', file);
+  let pkg_ = loadJsonFile.sync(file);
+  let ver = pkg_.version;
+  ver = semver.inc(ver, "patch");
+  console.log('ver ====', ver)
+  pkg_.version = ver;
+  writePkg(file, pkg_);
 }
