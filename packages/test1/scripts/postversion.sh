8955b6e059146d14307dacf876666fc7a07e4d48
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-07-30 15:41:20 +0800

    chore: test

--- packages/test1/scripts/postversion.sh
+++ packages/test1/scripts/postversion.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 if [[ -z "$(git tag --points-at HEAD | grep templates)" && ! -z "$(git diff HEAD^ -- ../../templates/package.json|grep version)" ]]
 then
+    echo "let me tag ==================="
     git tag "templates@$(node -p "require('../../templates/package.json').version")"
 fi
