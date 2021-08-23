2d4fdd17a3c0d12e74b267affffb7aebacf538c0
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-08-23 14:39:12 +0800

    chore: udpate

--- .github/scripts/simpleauth-azdo-pipelines.sh
+++ .github/scripts/simpleauth-azdo-pipelines.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 countNum=1000
 restUrl="https://dev.azure.com/mseng/VSIoT/_apis/build/latest/$2?api-version=6.0-preview.1"
+echo "============ step1" 
+echo $restUrl
 rsp=$(curl -u :$1 $restUrl)
 status=$(echo $rsp | jq -r '.status')
 buildId=$(echo $rsp | jq -r '.buildNumber')
@@ -9,6 +11,7 @@ while [[ $countNum -le 5000 && "$status" != "completed" ]]
 do 
     sleep $countNum
     status=$(curl -u :$1 $restUrl | jq -r '.status')
+    echo "=========== step2"
     countNum=$(( $countNum + 1000 ))
 done
 if [[ "$status" != "completed" ]]
@@ -18,4 +21,6 @@ fi
 
 restUrl="https://dev.azure.com/mseng/VSIoT/_apis/build/builds/$buildId/artifacts?api-version=6.0"
 asset_id=$(curl -u :$1 $restUrl | jq '.value |.[] | .resource' |jq '.data' | tr -d -c 0-9)
+echo "=============== step3"
+echo $restUrl
 echo $asset_id
