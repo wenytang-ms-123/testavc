05b2c77f792e3015cb595ae01b3b2b02a81e37d2
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-08-24 13:31:15 +0800

    chore: update

--- .github/scripts/simpleauth-azdo-pipelines.sh
+++ .github/scripts/simpleauth-azdo-pipelines.sh
@@ -2,23 +2,18 @@
 countNum=1
 # sleep 100
 restUrl="https://dev.azure.com/mseng/VSIoT/_apis/pipelines/$2/runs?api-version=6.0-preview.1"
-echo "============ step1" 
 
 rsp=$(curl -u :$1 $restUrl | jq -r '.value| .[0]')
 # status=$(echo $rsp | jq -r '.state')
 # buildId=$(echo $rsp | jq -r '.id')
 buildId=15836750
 status="completed"
-echo $status
 while [[ $countNum -le 50 && "$status" != "completed" ]]
 do 
-    echo "----------"
     sleep 1m
     rsp=$(curl -u :$1 $restUrl | jq -r '.value| .[0]')
     status=$(echo $rsp | jq -r '.state')
-    echo "=========== step2"
     countNum=$(( $countNum + 1 ))
-    echo $status
 done
 if [[ "$status" != "completed" ]]
 then
@@ -28,6 +23,4 @@ fi
 restUrl="https://dev.azure.com/mseng/VSIoT/_apis/build/builds/$buildId/artifacts?api-version=6.0"
 asset_rsp=$(curl -u :$1 $restUrl)
 asset_id=$(echo $asset_rep | jq '.value |.[] | .resource' |jq '.data' | tr -d -c 0-9)
-echo "=============== step3"
-echo $restUrl
 echo $asset_id
