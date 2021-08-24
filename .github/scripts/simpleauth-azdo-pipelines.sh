f9f2f863ebec3cf0d4bcd024992730f92a50bd78
wneytang-ms-123 <wenytang@wenytang.onmicrosoft.com>
2021-08-24 11:48:51 +0800

    chore: update

--- .github/scripts/simpleauth-azdo-pipelines.sh
+++ .github/scripts/simpleauth-azdo-pipelines.sh
@@ -4,15 +4,15 @@ countNum=1000
 restUrl="https://dev.azure.com/mseng/VSIoT/_apis/pipelines/11303/runs?api-version=6.0-preview.1"
 echo "============ step1" 
 
-rsp=$(curl -u :$1 $restUrl)
-rsp=$(echo $rsp| jq -r '.value| .[0]')
+rsp=$(curl -u :$1 $restUrl | jq -r '.value| .[0]')
 status=$(echo $rsp | jq -r '.state')
 buildId=$(echo $rsp | jq -r '.id')
 
 while [[ $countNum -le 5000 && "$status" != "completed" ]]
 do 
     sleep $countNum
-    status=$(curl -u :$1 $restUrl | jq -r '.state')
+    rsp=$(curl -u :$1 $restUrl | jq -r '.value| .[0]')
+    status=$(echo $rsp | jq -r '.state')
     echo "=========== step2"
     countNum=$(( $countNum + 1000 ))
 done
